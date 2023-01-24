0ea76d357cec40dca6192a4d2326758811721ef4
Marcos Pedraza <marcos.pedraza@terrabionic.com.mx>
2023-01-23 22:43:25 -0600

    update OnBackPressedCallback

--- app/src/main/java/com/marcospb/peliculasapp/MainActivity.kt
+++ app/src/main/java/com/marcospb/peliculasapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.marcospb.peliculasapp
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -40,5 +41,13 @@ class MainActivity : AppCompatActivity() {
 
         NavigationUI.setupActionBarWithNavController(this, navController)
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return true
     }
 }
--- app/src/main/java/com/marcospb/peliculasapp/ui/detail_movie/DetailFragment.kt
+++ app/src/main/java/com/marcospb/peliculasapp/ui/detail_movie/DetailFragment.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.marcospb.domain.utils.Result
 import com.marcospb.peliculasapp.R
@@ -47,6 +49,14 @@ class DetailFragment : Fragment() {
         val idMovie = DetailFragmentArgs.fromBundle(requireArguments()).idMovie
         viewModel.getDetailMovieById(idMovie)
         viewModel.getDetailState().observe(viewLifecycleOwner, ::OnViewState)
+
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
     }
 
     private fun OnViewState(uiDetailState: DetailViewModel.UiDetailState) {
